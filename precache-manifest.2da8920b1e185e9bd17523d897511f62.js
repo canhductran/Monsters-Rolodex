34dbf0c4935ca30aa08a365f4bc46cf825742271
Canh Duc Tran <CHRIS@Canhs-MacBook-Pro.local>
2019-09-07 19:07:37 +1000

    Updates

---
+++ precache-manifest.2da8920b1e185e9bd17523d897511f62.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e1302f64295dbb2cc46662301368e9e",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "96c9a7015182d9a41404",
+    "url": "/monsters-rolodex/static/css/main.06dca109.chunk.css"
+  },
+  {
+    "revision": "ebdf587097504289c06e",
+    "url": "/monsters-rolodex/static/js/2.3adefb50.chunk.js"
+  },
+  {
+    "revision": "96c9a7015182d9a41404",
+    "url": "/monsters-rolodex/static/js/main.c44b5c27.chunk.js"
+  },
+  {
+    "revision": "3eb51d48b619f371fccd",
+    "url": "/monsters-rolodex/static/js/runtime~main.877b8247.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/monsters-rolodex/static/media/logo.5d5d9eef.svg"
+  }
+]);
